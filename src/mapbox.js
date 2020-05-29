7e8e3f7c69bf040016dc9a89a67040274ac1ed0c
gloer <gloer@langslet.com>
2020-05-29 15:16:19 +0200

    First commit

---
+++ src/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from 'mapbox-gl';
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = "pk.eyJ1IjoiZ2xvZXIiLCJhIjoiY2l2cm4ya3hxMDAyYTJ6bHU5MzZjcHNnbSJ9.PmFpGo0iUpws5YtIBnzVBQ";
+
+const key = {};
+
+export { mapbox, key };
